7f0d55065403e88159324f56dbe6bfb6da998945
GGaleote <gabrielgaleote@gmail.com>
2019-10-26 18:09:09 +0100

    Final tuning and final code 26-10-2019

--- Assignment1_VoiceProcessing/code/harmonic_enhancer - copia.py
+++ Assignment1_VoiceProcessing/code/harmonic_enhancer - copia.py
@@ -5,25 +5,24 @@ import matplotlib.pylab as plt
 import numpy as np
 import scipy.io.wavfile as wav
 import wave
-plt.rcParams['font.size'] = 14
 
 fs, soundwave = wav.read ('original.wav') # Read sound signal
 
 # Representation of Time Domain
-t = np.linspace (0, len(soundwave)/fs, len(soundwave)) # time vector
+t = np.linspace(0, len(soundwave)/fs, len(soundwave))  # time vector
 
 # ---- Figure 1 ------
 plt.figure(1)
 plt.plot(t, soundwave)
 plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
+plt.ylabel('Magnitude')
 
 # ---- Frequency Domain Calculations ----
 fftSoundWave = np.fft.fft(soundwave)
 f = np.linspace(0, fs, len(fftSoundWave)) # Full spectrum frequency range
 
 fftSoundwaveHalf = fftSoundWave[:len(fftSoundWave) // 2]  # REMOVE  the half of the mirrored spectrum
-fHalf = np.linspace(0, fs / 2, len(fftSoundwaveHalf)) # Half spectrum requency range
+fHalf = np.linspace(0, fs / 2, len(fftSoundwaveHalf)) # Half spectrum frequency range
 
 # ---- Figure 2 ----
 # FFT signal in linear and logarithmic axis
@@ -31,7 +30,7 @@ plt.figure(2)
 ax = plt.subplot(211)
 ax.plot(fHalf, abs(fftSoundwaveHalf))
 plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
+plt.ylabel('Samples')
 ax = plt.subplot(212)
 ax.plot(fHalf, 20*np.log10(abs(fftSoundwaveHalf)))
 plt.xlabel('Frequency (Hz)')
@@ -51,75 +50,57 @@ the conversion needs to be parsed to int obtaining the position of the fft signa
 
 """
 
-fLow = 5500  # Hz
+fLow = 7000  # Hz
 fHigh = fs/2  # Hz
-fAmplifyL = 1250 # Hz
-fAmplifyH = 2000  # Hz
-acoef = 3  # amplification rate
+fAmplifyL1 = 1700  # Hz
+fAmplifyH1 = 2400  # Hz
+fAmplifyL2 = 3400  # Hz
+fAmplifyH2 = 3800  # Hz
+a1 = 8  # amplification 1st harmonic
+a2 = 8  # amplification 2nd harmonic
 
-fL = int((fLow / fs ) * len(fftSoundWave))
-fH = int((fHigh / fs ) * len(fftSoundWave))
+# Conversion from frequency to sample
+fL = int((fLow / fs) * len(fftSoundWave))
+fH = int((fHigh / fs) * len(fftSoundWave))
 
-fAL = int((fAmplifyL / fs ) * len(fftSoundWave))
-fAH = int((fAmplifyH / fs ) * len(fftSoundWave))
+fAL1 = int((fAmplifyL1 / fs) * len(fftSoundWave))
+fAH1 = int((fAmplifyH1 / fs) * len(fftSoundWave))
 
-FSW = fftSoundWave  # pass fftSoundWave to other variable to not overlap the original signal
-
-# Operations
-
-
-FSW[fAL:fAH] = FSW[fAL:fAH] * acoef
-FSW[len(FSW) - fAH:len(FSW) - fAL] = FSW[len(FSW) - fAH:len(FSW) - fAL] * acoef
-FSW[fL:fH] = FSW[fL:fH] * 0.1
-FSW[len(FSW) - fH:len(FSW) - fL] = FSW[len(FSW) - fH:len(FSW) - fL] * 0.1
-
-
-# ----------------------
-"""
-fLow = 0  # Hz
-fHigh = 50  # Hz
-fAmplifyL = 4000  # Hz
-fAmplifyH = 22000 # Hz
-acoef = 0  # amplification rate
-
-fL = int((fLow / fs ) * len(fftSoundWave))
-fH = int((fHigh / fs ) * len(fftSoundWave))
-
-fAL = int((fAmplifyL / fs ) * len(fftSoundWave))
-fAH = int((fAmplifyH / fs ) * len(fftSoundWave))
+fAL2 = int((fAmplifyL2 / fs) * len(fftSoundWave))
+fAH2 = int((fAmplifyH2 / fs) * len(fftSoundWave))
 
 FSW = fftSoundWave  # pass fftSoundWave to other variable to not overlap the original signal
 
 # Operations
 
-
-FSW[fAL:fAH] = FSW[fAL:fAH] * acoef
-FSW[len(FSW) - fAH:len(FSW) - fAL] = FSW[len(FSW) - fAH:len(FSW) - fAL] * acoef
 FSW[fL:fH] = FSW[fL:fH] * 0
 FSW[len(FSW) - fH:len(FSW) - fL] = FSW[len(FSW) - fH:len(FSW) - fL] * 0
-"""
-# ---------------
 
+FSW[fAL1:fAH1] = FSW[fAL1:fAH1] * a1
+FSW[len(FSW) - fAH1:len(FSW) - fAL1] = FSW[len(FSW) - fAH1:len(FSW) - fAL1] * a1
 
+FSW[fAL2:fAH2] = FSW[fAL2:fAH2] * a2
+FSW[len(FSW) - fAH2:len(FSW) - fAL2] = FSW[len(FSW) - fAH2:len(FSW) - fAL2] * a2
 
-
-
-
+# Reconstruction of the signal
+timeFilteredSoundWave = np.fft.ifft(FSW)
+timeFilteredSoundWave = np.real(timeFilteredSoundWave)
 
 # ---- Figure 3 ----
 # Represents the result signal in linear and logarithmic axis.
 plt.figure(3)
+
 plt.subplot(211)
 plt.plot(fHalf, abs(FSW[:len(FSW) // 2]))
 plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-timeFilteredSoundWave = np.fft.ifft(FSW)
-timeFilteredSoundWave = np.real(timeFilteredSoundWave)
+plt.ylabel('Magnitude ')
 plt.subplot(212)
+
 plt.plot(fHalf, 20*np.log10(abs(FSW[:len(FSW) // 2])))
 plt.xscale('log')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude (dB)')
+
 # ---- Save Processed Sound Recording ----
 f = wave.open(r"improved.wav", "wb")
 # 1 channel with a sample width of 2 and frame rate of 2*fs which is standard
@@ -134,14 +115,22 @@ f.writeframes(timeFilteredSoundWave.tostring())
 # This representation is very useful to analyse the profile of the sound and the frequencies distribution
 # along the time. Thus, frequency over time is represented.
 plt.figure(4)
-plt.subplot(211)
+plt.subplot(311)
 plt.plot(t, soundwave)
 plt.xlabel('time (s)')
 plt.xlim([0, len(t)/fs])
 plt.ylabel('amplitude (samples)')
-plt.subplot(212)
+plt.subplot(312)
 powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(soundwave, Fs=fs)
 plt.xlabel('Time')
 plt.ylabel('Frequency')
+plt.subplot(313)
+powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(timeFilteredSoundWave, Fs=fs)
+plt.xlabel('Time')
+plt.ylabel('Frequency')
+
+plt.figure(5)
+plt.plot(t,timeFilteredSoundWave)
+
 
 plt.show()
--- Assignment1_VoiceProcessing/code/harmonic_enhancer.py
+++ Assignment1_VoiceProcessing/code/harmonic_enhancer.py
@@ -5,11 +5,12 @@ import matplotlib.pylab as plt
 import numpy as np
 import scipy.io.wavfile as wav
 import wave
+plt.rcParams.update({'font.size': 20})
 
-fs, soundwave = wav.read ('original2.wav') # Read sound signal
+fs, soundwave = wav.read ('original.wav') # Read sound signal
 
 # Representation of Time Domain
-t = np.linspace (0, len(soundwave)/fs, len(soundwave)) # time vector
+t = np.linspace(0, len(soundwave)/fs, len(soundwave))  # time vector
 
 # ---- Figure 1 ------
 plt.figure(1)
@@ -22,17 +23,12 @@ fftSoundWave = np.fft.fft(soundwave)
 f = np.linspace(0, fs, len(fftSoundWave)) # Full spectrum frequency range
 
 fftSoundwaveHalf = fftSoundWave[:len(fftSoundWave) // 2]  # REMOVE  the half of the mirrored spectrum
-fHalf = np.linspace(0, fs / 2, len(fftSoundwaveHalf)) # Half spectrum requency range
+fHalf = np.linspace(0, fs / 2, len(fftSoundwaveHalf)) # Half spectrum frequency range
 
 # ---- Figure 2 ----
 # FFT signal in linear and logarithmic axis
 plt.figure(2)
-ax = plt.subplot(211)
-ax.plot(fHalf, abs(fftSoundwaveHalf))
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Samples')
-ax = plt.subplot(212)
-ax.plot(fHalf, 20*np.log10(abs(fftSoundwaveHalf)))
+plt.plot(fHalf, 20*np.log10(abs(fftSoundwaveHalf)))
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude (dB)')
 plt.xscale('log')
@@ -50,74 +46,50 @@ the conversion needs to be parsed to int obtaining the position of the fft signa
 
 """
 
-fLow = 0 # Hz
-fHigh = 0  # Hz
-fAmplifyL = 1000  # Hz
-fAmplifyH = 2000  # Hz
-acoef = 3  # amplification rate
+fLow = 7000  # Hz
+fHigh = fs/2  # Hz
+fAmplifyL1 = 1700  # Hz
+fAmplifyH1 = 2400  # Hz
+fAmplifyL2 = 3400  # Hz
+fAmplifyH2 = 3800  # Hz
+a1 = 8  # amplification 1st harmonic
+a2 = 8  # amplification 2nd harmonic
 
-fL = int((fLow / fs ) * len(fftSoundWave))
-fH = int((fHigh / fs ) * len(fftSoundWave))
+# Conversion from frequency to sample
+fL = int((fLow / fs) * len(fftSoundWave))
+fH = int((fHigh / fs) * len(fftSoundWave))
 
-fAL = int((fAmplifyL / fs ) * len(fftSoundWave))
-fAH = int((fAmplifyH / fs ) * len(fftSoundWave))
+fAL1 = int((fAmplifyL1 / fs) * len(fftSoundWave))
+fAH1 = int((fAmplifyH1 / fs) * len(fftSoundWave))
 
-FSW = fftSoundWave  # pass fftSoundWave to other variable to not overlap the original signal
-
-# Operations
-
-FSW[fAL:fAH] = FSW[fAL:fAH] * acoef
-FSW[len(FSW) - fAH:len(FSW) - fAL] = FSW[len(FSW) - fAH:len(FSW) - fAL] * acoef
-FSW[fL:fH] = FSW[fL:fH] * 0
-FSW[len(FSW) - fH:len(FSW) - fL] = FSW[len(FSW) - fH:len(FSW) - fL] * 0
-
-
-# ----------------------
-"""
-fLow = 0  # Hz
-fHigh = 50  # Hz
-fAmplifyL = 4000  # Hz
-fAmplifyH = 22000 # Hz
-acoef = 0  # amplification rate
-
-fL = int((fLow / fs ) * len(fftSoundWave))
-fH = int((fHigh / fs ) * len(fftSoundWave))
-
-fAL = int((fAmplifyL / fs ) * len(fftSoundWave))
-fAH = int((fAmplifyH / fs ) * len(fftSoundWave))
+fAL2 = int((fAmplifyL2 / fs) * len(fftSoundWave))
+fAH2 = int((fAmplifyH2 / fs) * len(fftSoundWave))
 
 FSW = fftSoundWave  # pass fftSoundWave to other variable to not overlap the original signal
 
 # Operations
 
-
-FSW[fAL:fAH] = FSW[fAL:fAH] * acoef
-FSW[len(FSW) - fAH:len(FSW) - fAL] = FSW[len(FSW) - fAH:len(FSW) - fAL] * acoef
 FSW[fL:fH] = FSW[fL:fH] * 0
 FSW[len(FSW) - fH:len(FSW) - fL] = FSW[len(FSW) - fH:len(FSW) - fL] * 0
-"""
-# ---------------
 
+FSW[fAL1:fAH1] = FSW[fAL1:fAH1] * a1
+FSW[len(FSW) - fAH1:len(FSW) - fAL1] = FSW[len(FSW) - fAH1:len(FSW) - fAL1] * a1
 
+FSW[fAL2:fAH2] = FSW[fAL2:fAH2] * a2
+FSW[len(FSW) - fAH2:len(FSW) - fAL2] = FSW[len(FSW) - fAH2:len(FSW) - fAL2] * a2
 
-
-
-
+# Reconstruction of the signal
+timeFilteredSoundWave = np.fft.ifft(FSW)
+timeFilteredSoundWave = np.real(timeFilteredSoundWave)
 
 # ---- Figure 3 ----
 # Represents the result signal in linear and logarithmic axis.
 plt.figure(3)
-plt.subplot(211)
-plt.plot(fHalf, abs(FSW[:len(FSW) // 2]))
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Magnitude ')
-timeFilteredSoundWave = np.fft.ifft(FSW)
-timeFilteredSoundWave = np.real(timeFilteredSoundWave)
-plt.subplot(212)
 plt.plot(fHalf, 20*np.log10(abs(FSW[:len(FSW) // 2])))
 plt.xscale('log')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude (dB)')
+
 # ---- Save Processed Sound Recording ----
 f = wave.open(r"improved.wav", "wb")
 # 1 channel with a sample width of 2 and frame rate of 2*fs which is standard
@@ -132,14 +104,18 @@ f.writeframes(timeFilteredSoundWave.tostring())
 # This representation is very useful to analyse the profile of the sound and the frequencies distribution
 # along the time. Thus, frequency over time is represented.
 plt.figure(4)
-plt.subplot(211)
+plt.subplot(311)
 plt.plot(t, soundwave)
-plt.xlabel('time (s)')
+plt.xlabel('Time (s)')
 plt.xlim([0, len(t)/fs])
-plt.ylabel('amplitude (samples)')
-plt.subplot(212)
+plt.ylabel(' Magnitude')
+plt.subplot(312)
 powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(soundwave, Fs=fs)
-plt.xlabel('Time')
-plt.ylabel('Frequency')
+plt.xlabel('Time (s)')
+plt.ylabel('Frequency (Hz)')
+plt.subplot(313)
+powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(timeFilteredSoundWave, Fs=fs)
+plt.xlabel('Time (s)')
+plt.ylabel('Frequency (Hz)')
 
 plt.show()
