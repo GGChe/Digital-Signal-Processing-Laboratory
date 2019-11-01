f8a441c22e47c47bcaf749a3a378ea11f2b7b70e
GGaleote <gabrielgaleote@gmail.com>
2019-11-01 17:55:49 +0000

    Low Pass Filter 1-11-2019

--- Assignment2_ECG/ecg_filter.py
+++ Assignment2_ECG/ecg_filter.py
@@ -32,45 +32,72 @@ plt.figure(2)
 plt.plot(f, 20*np.log10(abs(ecg_fft)))
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude (dB)')
-plt.xscale('log')
+# plt.xscale('log')
 
+M = 100
+f_resp=np.ones(100)
+# note we need to add "+1" to the end because the end of
+# the range is not included.
+f_resp[4:6+1]=0
+f_resp[94:96+1]=0
+hc=np.fft.ifft(f_resp)
+h=np.real(hc)
+# this is from index 0 to index 49 on the left
+# and on the right hand side from index 50 to index 99
+h_shift = np.zeros(100)
+h_shift[0:50]=h[50:100]
+h_shift[50:100]=h[0:50]
+h_wind=h_shift * np.hamming(100)
+
+f_resp2=np.ones(100)
+# note we need to add "+1" to the end because the end of
+# the range is not included.
+f_resp2[2:50]=0
+f_resp2[50:98]=0
+hc2=np.fft.ifft(f_resp2)
+h2=np.real(hc2)
+# this is from index 0 to index 49 on the left
+# and on the right hand side from index 50 to index 99
+h_shift2 = np.zeros(100)
+h_shift2[0:50] = h2[50:100]
+h_shift2[50:100] = h2[0:50]
+h_wind2=h_shift2 * np.hamming(100)
+
+y2 = signal.lfilter(h_wind2, 1, ch1)
+plt.figure(3)
+plt.plot(y2)
+plt.show()
+
+"""
+# Code from lectures
 # filter
 M = 200
 k1 = int(45/fs * M)
 k2 = int(55/fs * M)
-
 x = np.ones(M)
-
 x[k1:k2+1] = 0
 x[M-k2:M-k1+1] = 0
 x = np.fft.ifft(x)
-
 x = np.real(x)
-
 h = np.zeros(M)
-
 h[0:int(M/2)] = x[int(M/2):M]
 h[int(M/2):M] = x[0:int(M/2)]
-
 h = h * np.hamming(M)
+"""
 
-y2 = signal.lfilter(h, 1, ch1)
-
-plt.figure(3)
-plt.plot(y2)
-
-plt.show()
+"""
 
 
 class FIR_filter:
-    def __init__(self, _coefficients):
-        self._coefficients=_coefficients
-        self.buffer=np.zeros(len(_coefficients))
-
-    def dofilter(self, v):
-
-        return result
-
+    def __init__(self,_coefficients):
+        self.ntaps = len(_coefficients)
+        self.coefficients = _coefficients
+        self.buffer = np.zeros(self.ntaps)
+    def dofilter(self,v):
+        buffer = np.roll(buffer,1)
+        self.buffer[0] = v
+        return np.inner(self.buffer,self.coefficients)
+"""
 """
 fs = 1000
 f1 = 45
