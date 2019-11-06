e6c36aef7fd1c78fcc3cefd8fb91d721ccfc4b34
GGaleote <gabrielgaleote@gmail.com>
2019-11-06 13:23:07 +0000

    Update from laboratory 6-11-2019

--- Assignment2_ECG/ecg_filter.py
+++ Assignment2_ECG/ecg_filter.py
@@ -37,43 +37,68 @@ plt.xscale('log')
 
 # -----------
 
+class RingBuffer:
+    def __init__(self, size):
+        self.data = [None for i in range(size)]
+
+    def append(self, x):
+        self.data.pop(0)
+        self.data.append(x)
+
+    def get(self):
+        return self.data
+
+
 class FIR_filter(object):
+    Buffer = 0
+    P = 0
+
     def __init__(self, ntaps, f0, f1, f2):
         self.ntaps = ntaps
         self.f0 = f0
         self.f1 = f1
         self.f2 = f2
+        self.Buffer = np.zeros(ntaps)
+        self.P = 0
 
-    def dofilter(self):
+    def dofilter(self, v):
         f_resp = np.ones(self.ntaps)
         # Limits for the filtering
         k0 = int((self.f0 / fs) * self.ntaps)
         k1 = int((self.f1 / fs) * self.ntaps)
         k2 = int((self.f2 / fs) * self.ntaps)
-        f_resp[k1:k2+1] = 0
-        f_resp[self.ntaps-k2:self.ntaps-k1+1] = 0
-        f_resp[0:k0+1] = 0
-        f_resp[self.ntaps-k0:self.ntaps] = 0
+        f_resp[k1:k2 + 1] = 0
+        f_resp[self.ntaps - k2:self.ntaps - k1 + 1] = 0
+        f_resp[0:k0 + 1] = 0
+        f_resp[self.ntaps - k0:self.ntaps] = 0
         hc = np.fft.ifft(f_resp)
         h = np.real(hc)
         h_shift = np.zeros(self.ntaps)
         h_shift[0:int(self.ntaps / 2)] = h[int(self.ntaps / 2):self.ntaps]
         h_shift[int(self.ntaps / 2):self.ntaps] = h[0:int(self.ntaps / 2)]
         # h_wind = h_shift * np.hamming(self.ntaps)
-        y = signal.lfilter(h_shift, 1, ECG)
+        self.Buffer[self.P] = v
+
+        if self.P == self.ntaps:
+            result = np.sum(self.Buffer[:] * h_shift[:])
+            self.P = 0
+
+        result = np.sum(self.Buffer[:] * h_shift[:])
+        self.P = self.P + 1
+
         plt.figure(3)
         plt.plot(y)
         return y
 
 
-classfilter = FIR_filter(200, 5, 45, 100)
-y = classfilter.dofilter()
+for i in np.linspace(0, , len(ECG)):
+    ecgin = ECG[i]
+    classfilter = FIR_filter(200, 1, 45, 500)
+    y = classfilter.dofilter(ecgin)
 
 plt.figure(4)
 plt.plot(20 * np.log10(np.fft.fft(y, fs)))
 plt.xscale('log')
 plt.show()
 
-
 np.savetxt("coeff12bit.dat", b)
-
