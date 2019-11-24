fc780b3e8356e7e08d106f9846a621d8b284bca1
GGChe <gabrielgaleote@gmail.com>
2019-11-24 18:17:44 +0000

    update code

--- Assignment3_IIR/IIR.py
+++ Assignment3_IIR/IIR.py
@@ -1,6 +1,18 @@
 import numpy as np
-import pylab as pl
+import matplotlib.pylab as plt
+import scipy.signal as signal
 
+data = np.loadtxt('IIR_pulse_read1.dat')
+
+samplingrate = 1000
+cutoff_frequency = 5
+
+fs = 100
+data_fft = np.fft.fft(data)
+f_axis = np.linspace(0, fs, len(data))  # Full spectrum frequency range
+plt.figure(1)
+plt.subplot(211)
+plt.plot(f_axis[:int(len(f_axis) / 2)], 20 * np.log10(abs(data_fft[:int(len(data_fft) / 2)])))
 
 # 2nd order IIR filter
 class IIR_filter:
@@ -9,6 +21,7 @@ class IIR_filter:
         self.a2 = _a2
         self.b0 = _b0
         self.b1 = _b1
+        self.b2 = 0
         self.buffer1 = 0
         self.buffer2 = 0
 
@@ -22,7 +35,7 @@ class IIR_filter:
 
 # Normalised frequency 0.1
 # T = 1
-f = 0.1
+f = 20
 
 # Q factor
 q = 10
@@ -30,23 +43,77 @@ q = 10
 # s infinite as defined for a 2nd order resonator (see impulse invar)
 si = np.complex(-np.pi * f / q, np.pi * f * np.sqrt(1 / (q ** 2)))
 
-# Coefficients
-b0 = 1
-b1 = -1
-a1 = np.real(-(np.exp(si)+np.exp(np.conjugate(si))))
-a2 = np.exp(2*np.real(si))
-
+# Calculate Coefficients
+b,a=signal.butter(2,0.1*2)
+b0 = b[0]
+b1=[1]
+a1=a[1]
+a2=a[2]
 f = IIR_filter(b0, b1, a1, a2)
 
+plt.plot(f)
+
+
+"""double b2 = 0;
+for(int i=0;i<1000000;i++) 
+{
+	float a=0;
+	if (i==10) a = 1;
+	b2 = b;
+    b = f.filter(a);
+	assert_print(!isnan(b),"Lowpass output is NAN\n");
+	if ((i>20) && (i<100))
+		assert_print((b != 0) || (b2 != 0),
+			     "Lowpass output is zero\n");
+}
+""" 
 x = np.zeros(100)
 x[10] = 1
-y = np.zeros(100)
+y = []
 
 for i in range(len(x)):
     y[i] = f.filter(x[i])
 
-pl.plot(y)
 
-pl.show()
+y_fft = np.fft.fft(y)
+
+print(len(y_fft))
+print(len(f_axis))
+
+plt.figure(1)
+plt.subplot(212)
+plt.plot(f_axis[:int(len(f_axis) / 2)], 20 * np.log10(abs(y_fft[:int(len(y_fft) / 2)])))
+
+
+plt.figure(3)
+
+# unfiltered
+plt.subplot(211)
+plt.plot(data)
+plt.xlabel('samples')
+plt.ylabel('unfiltered/raw ADC units')
+# filtered
+plt.subplot(212)
+plt.plot(y)
+plt.xlabel('samples')
+plt.ylabel('filtered/raw ADC units')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
